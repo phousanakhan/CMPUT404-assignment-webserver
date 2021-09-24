5c0239d7b859191a663fb0e7766976715aa51600
Phousanak Han <phousana@ualberta.ca>
2021-09-24 13:05:15 -0600

    update license: add Phousanak Han

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import socketserver
 import os
 from time import strftime, gmtime
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Copyright 2013 Abram Hindle, Eddie Antonio Santos, Phousanak Han
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
